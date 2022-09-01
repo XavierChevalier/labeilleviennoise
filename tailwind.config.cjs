01887394e2a4de89968f86e0f127f122e0815b78
Bloodysunset <chevalier.nicolas2103@gmail.com>
2022-09-01 20:11:56 +0200

    feat(home): add homepage
    Co-authored-by: XavierChevalier <mcmodz@hotmail.fr>
    Closes: #1

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,8 +2,28 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    colors: {
+      black: {
+        DEFAULT: '#000',
+        light: '#232221',
+      },
+      white: '#fff',
+      primary: {
+        DEFAULT: '#bd773f',
+        light: '#dfcfc5',
+      },
+      secondary: {
+        DEFAULT: '#d4b97f',
+        light: '#e5e1d7',
+      },
+      tertiary: {
+        DEFAULT: '#71502f',
+        light: '#d5c6b1',
+      },
+    },
   },
   plugins: [],
 }
-
