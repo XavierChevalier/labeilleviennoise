01f61f1660a09f641b4c2b9ecd483ccad3074711
Bloodysunset <chevalier.nicolas2103@gmail.com>
2022-09-17 17:35:41 +0200

    chore: add 'deps' type for commit
    Co-authored-by: Xavier Chevalier <XavierChevalier@users.noreply.github.com>

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,21 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'chore',
+        'docs',
+        'style',
+        'refactor',
+        'test',
+        'revert',
+        'deps',
+      ],
+    ],
     'scope-enum': [2, 'always', []],
     'footer-max-line-length': [0, 'always'], // Make sure there is never a max-line-length by disabling the rule
   },
