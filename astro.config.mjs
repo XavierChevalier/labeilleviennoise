fedeac363ce552668f5bf781337d8eed8971c23b
XavierChevalier <mcmodz@hotmail.fr>
2022-09-23 14:59:37 +0200

    chore: add ESLint plugin for Astro components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
+import image from '@astrojs/image'
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
-import image from '@astrojs/image'
 
 // https://astro.build/config
 export default defineConfig({
