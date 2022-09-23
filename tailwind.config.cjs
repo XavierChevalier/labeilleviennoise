41e14b744f4013148ff0594020a1977086caa543
Bloodysunset <chevalier.nicolas2103@gmail.com>
2022-09-23 11:58:48 +0200

    feat: homepage setup
    Co-authored-by: Xavier Chevalier <XavierChevalier@users.noreply.github.com>

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,31 +1,37 @@
 /** @type {import('tailwindcss').Config} */
+
+const { default: colors } = require('tailwindcss/colors')
+
 module.exports = {
+  darkMode: 'class',
   content: [
     './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
     './node_modules/flowbite/**/*.js',
   ],
   theme: {
+    extend: {
+      colors: {
+        black: {
+          DEFAULT: '#000',
+          light: '#232221',
+        },
+        white: '#fff',
+        primary: {
+          DEFAULT: '#bd773f',
+          light: '#dfcfc5',
+        },
+        secondary: {
+          DEFAULT: '#d4b97f',
+          light: '#e5e1d7',
+        },
+        tertiary: {
+          DEFAULT: '#71502f',
+          light: '#d5c6b1',
+        },
+      },
+    },
     container: {
       center: true,
-    },
-    colors: {
-      black: {
-        DEFAULT: '#000',
-        light: '#232221',
-      },
-      white: '#fff',
-      primary: {
-        DEFAULT: '#bd773f',
-        light: '#dfcfc5',
-      },
-      secondary: {
-        DEFAULT: '#d4b97f',
-        light: '#e5e1d7',
-      },
-      tertiary: {
-        DEFAULT: '#71502f',
-        light: '#d5c6b1',
-      },
     },
   },
   plugins: [require('flowbite/plugin')],
