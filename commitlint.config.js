4705f70059b77ac5b9c435ee15f042d6bed47797
xavierchevalier <mcmodz@hotmail.fr>
2022-11-20 14:34:11 +0100

    feat(faq): add more topic

--- commitlint.config.js
+++ commitlint.config.js
@@ -22,7 +22,7 @@ module.exports = {
     'scope-enum': [
       2,
       'always',
-      ['apiary', 'home', 'honey-house', 'sponsorship'],
+      ['apiary', 'home', 'honey-house', 'faq', 'sponsorship'],
     ],
     'footer-max-line-length': [0, 'always'], // Make sure there is never a max-line-length by disabling the rule
   },
