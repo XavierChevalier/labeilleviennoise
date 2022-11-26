af3b454a27a8a3405ffb96ff7815d011168df3ac
Bloodysunset <chevalier.nicolas2103@gmail.com>
2022-11-26 17:34:34 +0100

    fix: better spacing inside table cells

--- app/modules/sponsorship/pricing/pricing-comparison-table.tsx
+++ app/modules/sponsorship/pricing/pricing-comparison-table.tsx
@@ -22,8 +22,8 @@ export default function PricingComparisonTable({ isToggled, formulas }: Props) {
       </thead>
       <tbody>
         <tr className="border-b-[1px]">
-          <td className="py-3 font-medium">Prix</td>
-          <td className="py-3">
+          <td className="px-2 py-3 font-medium">Prix</td>
+          <td className="px-2 py-3">
             <PeriodLabel
               pricePerMonth={nectar.pricePerMonth}
               pricePerYear={nectar.pricePerYear}
@@ -31,7 +31,7 @@ export default function PricingComparisonTable({ isToggled, formulas }: Props) {
             />
             <ButtonChosePlan title={nectar.title} isToggled={isToggled} />
           </td>
-          <td className="py-3">
+          <td className="px-2 py-3">
             <PeriodLabel
               pricePerMonth={propolis.pricePerMonth}
               pricePerYear={propolis.pricePerYear}
@@ -39,7 +39,7 @@ export default function PricingComparisonTable({ isToggled, formulas }: Props) {
             />
             <ButtonChosePlan title={propolis.title} isToggled={isToggled} />
           </td>
-          <td className="py-3">
+          <td className="px-2 py-3">
             <PeriodLabel
               pricePerMonth={geleeRoyale.pricePerMonth}
               pricePerYear={geleeRoyale.pricePerYear}
@@ -49,10 +49,10 @@ export default function PricingComparisonTable({ isToggled, formulas }: Props) {
           </td>
         </tr>
         <tr className="even:bg-gray-100/25">
-          <td className="py-3 font-medium">Abeilles préservées</td>
-          <td className="py-3">5.000</td>
-          <td className="py-3">20.000</td>
-          <td className="py-3">
+          <td className="px-2 py-3 font-medium">Abeilles préservées</td>
+          <td className="px-2 py-3">5.000</td>
+          <td className="px-2 py-3">20.000</td>
+          <td className="px-2 py-3">
             40.000
             <small className="block text-sm text-gray-500">
               soit une ruche entière
@@ -60,60 +60,62 @@ export default function PricingComparisonTable({ isToggled, formulas }: Props) {
           </td>
         </tr>
         <tr className="even:bg-gray-100/25">
-          <td className="py-3 font-medium">Fleurs/Arbres planté(e)s</td>
-          <td className="py-3">
+          <td className="px-2 py-3 font-medium">Fleurs/Arbres planté(e)s</td>
+          <td className="px-2 py-3">
             5.000 fleurs plantées
             <small className="block text-sm text-gray-500">
               1 fleur = 1 abeille heureuse
             </small>
           </td>
-          <td className="py-3">
+          <td className="px-2 py-3">
             <span className="font-semibold">Formule Nectar +</span>
             <br />1 arbre planté
             <small className="block text-sm text-gray-500">par an</small>
           </td>
-          <td className="py-3">
+          <td className="px-2 py-3">
             <span className="font-semibold">Formule Nectar +</span>
             <br />4 arbres plantés
             <small className="block text-sm text-gray-500">par an</small>
           </td>
         </tr>
         <tr className="even:bg-gray-100/25">
-          <td className="py-3 font-medium">Pot de miel</td>
-          <td className="py-3">
+          <td className="px-2 py-3 font-medium">Pot de miel</td>
+          <td className="px-2 py-3">
             1 pot de miel de 500g
             <small className="block text-sm text-gray-500">
               par an, étiquette personnalisée
             </small>
           </td>
-          <td className="py-3">
+          <td className="px-2 py-3">
             <span className="font-semibold">Formule Nectar +</span>
             <br />4 pots de miel de 250g
             <small className="block text-sm text-gray-500">
               1 par trimestre
             </small>
           </td>
-          <td className="py-3">
+          <td className="px-2 py-3">
             <span className="font-semibold">Formule Nectar & Propolis +</span>
             <br />4 pots de miel de 250g
             <small className="block text-sm text-gray-500">par an</small>
           </td>
         </tr>
         <tr className="even:bg-gray-100/25">
-          <td className="py-3 font-medium">Mascotte L'Abeille Viennoise™</td>
-          <td className="py-3">
+          <td className="px-2 py-3 font-medium">
+            Mascotte L'Abeille Viennoise™
+          </td>
+          <td className="px-2 py-3">
             1
             <small className="block text-sm text-gray-500">
               valeur approximative 20€
             </small>
           </td>
-          <td className="py-3">
+          <td className="px-2 py-3">
             1
             <small className="block text-sm text-gray-500">
               valeur approximative 40€
             </small>
           </td>
-          <td className="py-3">
+          <td className="px-2 py-3">
             2
             <small className="block text-sm text-gray-500">
               celles des formules précédentes
@@ -121,41 +123,41 @@ export default function PricingComparisonTable({ isToggled, formulas }: Props) {
           </td>
         </tr>
         <tr className="even:bg-gray-100/25">
-          <td className="py-3 font-medium">
-            Des nouvelles des abeilles accompagnée de photos pour suivre
+          <td className="px-2 py-3 font-medium">
+            Des nouvelles des abeilles accompagnées de photos pour suivre
             l'évolution des ruches
             <small className="block text-sm text-gray-500">par trimestre</small>
           </td>
-          <td className="py-3">
+          <td className="px-2 py-3">
             <IconCheck />
           </td>
-          <td className="py-3">
+          <td className="px-2 py-3">
             <IconCheck />
           </td>
-          <td className="py-3">
+          <td className="px-2 py-3">
             <IconCheck />
           </td>
         </tr>
         <tr className="even:bg-gray-100/25">
-          <td className="py-3 font-medium">
+          <td className="px-2 py-3 font-medium">
             Coffret gourmand surprise
             <small className="block text-sm text-gray-500">par an</small>
           </td>
-          <td className="text-red-500 font-black">─</td>
-          <td className="py-3">
+          <td className="px-2 py-3 text-red-500 font-black">─</td>
+          <td className="px-2 py-3">
             <IconCheck />
           </td>
-          <td className="py-3">
+          <td className="px-2 py-3">
             <IconCheck />
           </td>
         </tr>
         <tr className="even:bg-gray-100/25">
-          <td className="py-3 font-medium">
+          <td className="px-2 py-3 font-medium">
             Plaque nominative sur votre ruche
           </td>
-          <td className="text-red-500 font-black">─</td>
-          <td className="text-red-500 font-black">─</td>
-          <td className="py-3">
+          <td className="px-2 py-3 text-red-500 font-black">─</td>
+          <td className="px-2 py-3 text-red-500 font-black">─</td>
+          <td className="px-2 py-3">
             <IconCheck />
           </td>
         </tr>
