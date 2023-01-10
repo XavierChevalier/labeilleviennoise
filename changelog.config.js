53d236871215171958442346544403e5b6da0675
xavierchevalier <mcmodz@hotmail.fr>
2023-01-10 22:00:44 +0100

    ci: add changelog config for changelog file generation

---
+++ changelog.config.js
@@ -0,0 +1,21 @@
+'use strict'
+const config = require('conventional-changelog-conventionalcommits')
+
+module.exports = config({
+  types: [
+    { type: 'feat', section: '🎉 New Features' },
+    { type: 'fix', section: '🐛 Bugfixes' },
+    { type: 'perf', section: '🚀 Performance Improvements' },
+    { type: 'build', section: '🛠 Build System' },
+    { type: 'ci', section: '🛠 Build System' },
+    { type: 'refactor', section: '🪚 Refactors' },
+    { type: 'docs', section: '📚 Documentation Changes' },
+    { type: 'test', section: '🧪 Tests' },
+    { type: 'style', section: '💎 Code Style Changes' },
+    { type: 'deps', section: '🧹 Chores' },
+    { type: 'chore', section: '🧹 Chores' },
+    { type: 'revert', section: '🗑 Reverts' },
+    { type: '*!', section: '⚠️ BREAKING CHANGES' },
+    { type: 'other', section: '💚 Other Changes' },
+  ],
+})
