ae1202aa70dbfe09261094ba7b46c475ea8513a2
xavierchevalier <mcmodz@hotmail.fr>
2023-01-10 11:47:54 +0100

    refactor: create dedicated config and cli packages directories

--- packages/build/src/build-library.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-sh ../../packages/build/node_modules/.bin/rimraf dist && \
-concurrently -n swc,tsc "sh ../../packages/build/node_modules/.bin/swc src -d dist" "sh ../../packages/build/node_modules/.bin/tsc" && \
-sh ../../packages/build/node_modules/.bin/tsc-alias
