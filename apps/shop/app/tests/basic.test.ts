64f3c724d8febb8cdda6bbff33665b2ddafe42b7
Bloodysunset <chevalier.nicolas2103@gmail.com>
2023-01-16 16:30:02 +0100

    feat(shop): liquid theme for the shop

--- apps/shop/app/tests/basic.test.ts
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-describe('tests', () => {
-  it('should works', () => {
-    expect(1 + 1).toBe(2)
-  })
-})
--- apps/shop/app/tests/setup/jest.setup.after-env.ts
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import * as matches from 'jest-extended'
-
-expect.extend(matches)
